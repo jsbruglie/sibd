c516b313556438bc0712d05675d7b03767fb8c92
dbsousa1 <danielbsousa1@gmail.com>
2017-11-12 15:15:02 +0000

    Fixed a bug

--- delivery2/db_query1.sql
+++ delivery2/db_query1.sql
@@ -1,7 +1,9 @@
 /* 1st. patient name with the highest number of readings of units of "LDL cholesterol in mg/DL" in the past 90 days*/
 
-SELECT name,units,datetime, max(value) FROM reading NATURAL JOIN wears, sensor, patient
+SELECT DISTINCT name,units,datetime, value FROM reading NATURAL JOIN wears, sensor, patient
 WHERE datetime BETWEEN start AND end
 AND wears.patient = patient.number
 AND DateDiff(current_date,cast(datetime as date)) <= 90
-AND sensor.units = 'LDL cholesterol in mg/dL';
+AND sensor.units = 'LDL cholesterol in mg/dL'
+AND value>=all((SELECT value FROM reading 
+	WHERE DateDiff(current_date,cast(datetime as date)) <= 90));
