bd5f41bd2b59cd9ea69dfa3be0aad15be8c44dfc
dbsousa1 <danielbsousa1@gmail.com>
2017-11-12 17:13:06 +0000

    Bugfix wrong patient was showing

--- delivery2/db_query1.sql
+++ delivery2/db_query1.sql
@@ -1,12 +1,12 @@
 /* 1st. patient name with the highest number of readings (higher thatn 200) of units of "LDL cholesterol in mg/DL" 
 in the past 90 days*/
 
-SELECT DISTINCT name,units, value FROM reading NATURAL JOIN wears, sensor, patient
+SELECT name,units, value FROM reading NATURAL JOIN wears, sensor, patient
 WHERE datetime BETWEEN start AND end
 AND wears.patient = patient.number
 AND DateDiff(current_date,cast(datetime as date)) <= 90
 AND sensor.units = 'LDL cholesterol in mg/dL'
-AND value >= 200
+AND value > 200
 AND value>=all((SELECT value FROM reading 
 	WHERE DateDiff(current_date,cast(datetime as date)) <= 90))
 GROUP BY name;
