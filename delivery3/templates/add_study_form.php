7f9b859bed545cdc9801e9e5bf88d0601501c281
Borrego <joao.borrego64@gmail.com>
2017-11-24 15:03:10 +0000

    add study and series insert structure

--- delivery3/templates/add_study_form.php
+++ delivery3/templates/add_study_form.php
@@ -1,33 +1,19 @@
 <form action="study.php" method="post">
     <h4>New Study</h4>
     <div class="form-group required row">
-        <div class="col-sm-4">
-            <label class="col-form-label" for="name">Date</label>
-            <input class="form-control" autofocus name="date" placeholder= "Study date" type="date"
+        <div class="col-sm-2">
+            <label class="col-form-label" for="name">Request ID</label>
+            <input class="form-control" autofocus name="request_number" placeholder= "Request ID" type="text"
                 <?php 
-                    if (isset($_POST['date'])) echo 'value="' . $_POST['date'] . '"';
-                    else if (isset($date)) echo 'value="' . $date . '"';
+                    if (isset($_POST['request_number'])) echo 'value="' . $_POST['request_number'] . '"';
+                    else if (isset($request_number)) echo 'value="' . $request_number . '"';
                 ?>
             />
-            <?php if (isset($date_err)): ?>
-            <span class="alert text-danger" role="alert"><?php echo $date_err;?>
+            <?php if (isset($request_number_err)): ?>
+            <span class="alert text-danger" role="alert"><?php echo $request_number_err;?>
             <?php endif ?></span>
         </div>
-        <div class="col">
-            <label class="col-form-label" for="description">Description</label>
-            <input class="form-control" name="description" placeholder="Study description" type="text"
-                <?php 
-                    if (isset($_POST['description'])) echo 'value="' . $_POST['description'] . '"';
-                    else if (isset($description)) echo 'value="' . $description . '"';
-                ?>
-            />
-            <?php if (isset($description_err)): ?>
-            <span class="alert text-danger" role="alert"><?php echo $description_err;?>
-            <?php endif ?></span>
-        </div>
-    </div>
-    <div class="form-group required row">
-        <div class="col sm-4">
+        <div class="col-sm-2">
             <label class="col-form-label" for="doctor_id">Doctor ID</label>
             <input class="form-control" name="doctor_id" placeholder="Doctor" type="text"
                 <?php 
@@ -64,9 +50,44 @@
             <?php endif ?></span>
         </div>
     </div>
+    <div class="form-group required row">
+         <div class="col-sm-4">
+            <label class="col-form-label" for="name">Date</label>
+            <input class="form-control" name="date" placeholder= "Study date" type="date"
+                <?php 
+                    if (isset($_POST['date'])) echo 'value="' . $_POST['date'] . '"';
+                    else if (isset($date)) echo 'value="' . $date . '"';
+                ?>
+            />
+            <?php if (isset($date_err)): ?>
+            <span class="alert text-danger" role="alert"><?php echo $date_err;?>
+            <?php endif ?></span>
+        </div>
+        <div class="col">
+            <label class="col-form-label" for="description">Description</label>
+            <input class="form-control" name="description" placeholder="Study description" type="text"
+                <?php 
+                    if (isset($_POST['description'])) echo 'value="' . $_POST['description'] . '"';
+                    else if (isset($description)) echo 'value="' . $description . '"';
+                ?>
+            />
+            <?php if (isset($description_err)): ?>
+            <span class="alert text-danger" role="alert"><?php echo $description_err;?>
+            <?php endif ?></span>
+        </div>
+    </div>
 
     <h4>New Series</h4>
     <div class="form-group required row">
+        <div class="col-sm-2">
+            <label class="col-form-label" for="series_id">Series ID</label>
+            <input class="form-control" name="series_id" placeholder= "Series" type="text"
+                <?php if (isset($_POST['series_id'])) echo 'value="' . $_POST['series_id'] . '"'; ?>
+            />
+            <?php if (isset($series_id_err)): ?>
+            <span class="alert text-danger" role="alert"><?php echo $series_id_err;?>
+            <?php endif ?></span>
+        </div>
         <div class="col-sm-4">
             <label class="col-form-label" for="series_name">Name</label>
             <input class="form-control" name="series_name" placeholder= "Series" type="text"
